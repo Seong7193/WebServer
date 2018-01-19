35e14da5703df2f622d0d264588f2820ce3d5548
Seong Han <hans@marc-hi.ca>
2018-01-19 10:33:31 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.rul}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        if(err){
+            console.log('Unable to find Server.log');
+        }
+    }); 
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+}); 
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        message: 'You failed me!',
+        reason: 'What\'s more reasons to give other than you just failed me?'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
